6c32a71e711b8e45ff12df1e46086fa2a2988933
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-11-26 15:59:30 +0900

    main

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+/// <reference path="../typings/bundle.d.ts" />
+
+// For export modules
+
+import ResourcePanel     from './code/resource-panel';
+import ItPanel           from './code/it-panel';
+import SelectableInput   from './code/selectable-input';
+import ArgLi             from './code/arg_li';
+import * as Info         from './info';
+import Vue = require('vue');
+
+Vue.component("resource-panel", ResourcePanel);
+Vue.component("it-panel", ItPanel);
+Vue.component("selectable-input", SelectableInput);
+Vue.component("arg-li", ArgLi);
+
+
+
+import VueMain from './code/ui';
+
+export const App = VueMain;
